656e8489bfb776227f65a2f964c968006345e8cb
Chris Ramer <chrisramer.97@gmail.com>
2020-09-23 16:08:47 -0700

    Add dice roll functionality for player

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,76 @@
 // Business Logic
 
+let playerName = "Steve";
 let turnCount = 0;
-let playerPts;
-let cpuPts;
-let playerDiceCount;
-let cpuDiceCount;
+let rollCount = 1;
+let playerPts = 0;
+let playerPtsThisTurn = 0;
+let cpuPts = 0;
+let cpuPtsThisTurn = 0;
+let playerDiceCount = 0;
+let cpuDiceCount = 0;
 
-function AddTurn() {
-	turnCount++;
+function StartGame() {
+	playerName = $("input#playerName").val();
+	//AddTurn();
+	OnRollEnd(); // Start game
+	$("#startGame").hide(); // Hide game setup
+	$("#playGame").fadeIn(500); // Show gameplay
 }
 
-const addPoints = function(points) {
+const IsPlayersTurn = function() {
+	if (turnCount % 2 == 1) {
+		return true;
+	}
+
+	return false;
+}
+
+function OnDiceRoll() {
+	const randNum = Math.floor(Math.random() * (6 - 1) + 1) // Roll random dice number
+	document.getElementById("diceRolled").src = "img/dice" + randNum + ".png"; // Update dice graphic
+
+	// If rolled a 1, lose all points
+	if (randNum === 1) {
+		playerPtsThisTurn = 0;
+	}
+	else {
+		if (IsPlayersTurn()) {
+			playerPtsThisTurn += randNum;
+			playerDiceCount++;
+		}
+		else {
+			cpuPtsThisTurn += randNum;
+			cpuDiceCount++;
+		}
+	}
+
+	OnRollEnd();
+}
+
+function OnRollEnd() {
+	if (IsPlayersTurn()) {
+		$("#playGame h3").text("Turn " + parseInt(turnCount) + " | Roll " + parseInt(rollCount++) + ": " + playerName + " Rolls!");
+	}
+	else {
+		$("#playGame h3").text("Turn " + parseInt(turnCount) + " | Roll " + parseInt(rollCount++) + ": CPU Rolls!");
+	}
+}
+
+function OnHold() {
+	if (IsPlayersTurn()) {
+		playerPts += playerPtsThisTurn;
+		playerDiceCount = 0;
+		playerPtsThisTurn = 0;
+	}
+	else {
+		cpuPts += cpuPtsThisTurn;
+		cpuDiceCount = 0;
+		cpuPtsThisTurn = 0;
+	}
+}
+
+function UpdateScore (points) {
 	// If player's turn
 	if (turnCount % 2 == 1) {
 		playerPts += points;
@@ -21,8 +81,16 @@ const addPoints = function(points) {
 	}
 }
 
-function GenerateDice() {
+function NextTurn() {
 	//
+}
+
+function CPUTurn() {
+	//
+}
+
+function AddTurn() {
+	turnCount++;
 }
 
 // User interface logic
@@ -33,11 +101,17 @@ $(document).ready(function () {
 		e.preventDefault();
 		StartGame();
 	});
+	
+	// On click roll again
+	$("#roll").click(function (e) { 
+		e.preventDefault();
+		OnDiceRoll();
+	});
 
-	function StartGame() {
-		AddTurn(); // Start on turn 1 (player)
-		$("#startGame").hide(); // Hide game setup
-		$("#playGame").show(); // Show gameplay
-	}
+	// On click hold
+	$("#hold").click(function (e) { 
+		e.preventDefault();
+		OnHold();
+	});
 
 });
